6bcf3cda7b4a529b3ea1a1fa016bb70373d44c5f
Fabio Sander <fabiosandernt@gmail.com>
2025-02-08 18:58:01 -0300

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,5 @@
 /**
-* Template Name: Vesperr - v4.9.0
-* Template URL: https://bootstrapmade.com/vesperr-free-bootstrap-template/
-* Author: BootstrapMade.com
-* License: https://bootstrapmade.com/license/
+
 */
 (function() {
   "use strict";
